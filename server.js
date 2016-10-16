3b6d852928df1bccd3d8ec284eeecbd383d00f74
Darshan <dave1089@live.com>
2016-10-16 00:38:14 +0000

    logic done

--- server.js
+++ server.js
@@ -1,8 +1,31 @@
 var express = require("express")
 var app = express();
-
+var unix = ""
+var natural = ""
 app.get('/:path', function (req, res) {
-  res.send(req.params.path);
+  var param = req.params.path
+  if(isNaN(param)){
+    var check = Date.parse(param);
+    if(isNaN(check)){
+      natural = null
+      unix = null 
+    }
+    else{
+    unix = new Date(param).getTime()/1000
+    natural = param
+    }
+  }
+  else {
+    unix = Number(param)
+    var change = new Date(param*1000)
+    var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+    var y = change.getFullYear();
+    var m = months[change.getMonth()]
+    var d = change.getDate()
+    natural = m + " " + d + "," + " " + y
+  }
+  var data = JSON.stringify({"unix" : unix , "natural" : natural})
+  res.send("Parameter: " + data);
 });
 
 app.listen(8080, function () {
